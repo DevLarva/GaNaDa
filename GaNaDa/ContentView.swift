422d7bddbd0b03b2f92106cca4917504e5c751e1
백대홍 <bdh3620@naver.com>
2022-09-27 01:00:04 +0900

    button bug fix

--- GaNaDa/ContentView.swift
+++ GaNaDa/ContentView.swift
@@ -12,11 +12,12 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
+                
                 Text("메인입니다.")
                     .padding()
                 
             }
-            .navigationTitle("Home")
+            .navigationTitle("Guess the Hangul!")
         }
         .fullScreenCover(isPresented: $shouldShowOnboarding, content: { OnboardingView(shouldShowOnboarding: $shouldShowOnboarding)
         })
@@ -28,31 +29,38 @@ struct OnboardingView: View {
     @Binding var shouldShowOnboarding: Bool
     var body: some View {
         TabView {
-            PageView(title: "asdasd",
-                     subtitle: "adsadasd",
-                     imageName: "king", showsDismissButton: false,
-                     shouldShowOnboarding: $shouldShowOnboarding
-            )
-                
-            PageView(title: "asdasd",
-                     subtitle: "adsadasd",
-                     imageName: "king", showsDismissButton: false,
-                     shouldShowOnboarding: $shouldShowOnboarding
-            
-            )
-                
-            
-            PageView(title: "asdasd",
-                     subtitle: "adsadasd",
-                     imageName: "king", showsDismissButton: false,
-                     shouldShowOnboarding: $shouldShowOnboarding
-            
-            )
-                
-            
-            PageView(title: "asdasd",
-                     subtitle: "adsadasd",
+            PageView(title: "How well do you know Korean?",
+                     subtitle: "Until now, it was not easy to learn Korean. But what about with GaNaDa?",
                      imageName: "king",
+                     subimageName:"gyeongbokgung-palace",
+                     showsDismissButton: false,
+                     shouldShowOnboarding: $shouldShowOnboarding
+            )
+                
+            PageView(title: "Learn",
+                     subtitle: "간단한 게임으로 다양한 한국어를 학습 해보세요",
+                     imageName: "student",
+                     subimageName:"south-korea",
+                     showsDismissButton: false,
+                     shouldShowOnboarding: $shouldShowOnboarding
+            
+            )
+            
+            
+            PageView(title: "블라블라",
+                     subtitle: "",
+                     imageName: "",
+                     subimageName:"",
+                     showsDismissButton: false,
+                     shouldShowOnboarding: $shouldShowOnboarding
+            
+            )
+                
+            
+            PageView(title: "블라블라",
+                     subtitle: "",
+                     imageName: "",
+                     subimageName:"",
                      showsDismissButton: true,
                      shouldShowOnboarding: $shouldShowOnboarding
             
@@ -70,41 +78,62 @@ struct PageView: View {
     let title: String
     let subtitle: String
     let imageName: String
+    let subimageName: String
     let showsDismissButton: Bool
     @Binding var shouldShowOnboarding: Bool
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 150, height: 150)
-                .padding()
-            
-            Text(title)
-                .font(.system(size: 32))
-                .padding()
-            
-            
-            Text(subtitle)
-                .font(.system(size: 24))
-                .foregroundColor(Color(.secondaryLabel))
-                .padding()
-            
-            if showsDismissButton {
-                Button(action: {
-                    shouldShowOnboarding.toggle() 
-                }, label: {
-                    Text("Get Start")
-                        .bold()
-                        .frame(width: 200, height: 50)
-                        .background(Color.green)
-                        .cornerRadius(6)
-                    
-                })
+        VStack(spacing: 20) {
+            ZStack {
                 
-            }
+     Spacer()
+     Spacer()
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200.0, height: 200.0)
+                    .offset(x: 0, y: 150)
+                    .overlay(
+                        Image(subimageName)
+                            .resizable()
+                            .scaledToFit()
+                            .offset(x: 0, y: -50)
+                        
+                    )
+                        } //이미지 두개 겹쳐 보이게 하기
+            
+               
+    
+                
+                Text(title)
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                
+        
+                Text(subtitle)
+                .font(.headline)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: 250)
+                .foregroundColor(Color(red: 237 / 255, green: 203 / 255, blue: 150 / 255))
+                .shadow(color: Color.black.opacity(0.1), radius: 1, x: 2, y: 2)
+            
+                if showsDismissButton {
+                    Button(action: {
+                        shouldShowOnboarding.toggle()
+                    }, label: {
+                        Text("Get Start")
+                            .bold()
+                            .frame(width: 200, height: 50)
+                            .background(Color.yellow)
+                            .cornerRadius(6)
+           
+                    })
+                }
             
         }
+        
     }
 }
 
