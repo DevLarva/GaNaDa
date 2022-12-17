5d62f7711be00328990d437cbc57f21e26f8d4dc
백대홍 <bdh3620@naver.com>
2022-12-17 11:37:26 +0900

    UITest-MemoView

---
+++ GaNaDa/UiTest.swift
@@ -0,0 +1,52 @@
+//
+//  UiTest.swift
+//  GaNaDaUITests
+//
+//  Created by 백대홍 on 2022/12/17.
+//
+
+import XCTest
+
+final class UiTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        XCUIApplication().launch()
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    func test_Vocabulary() {
+                        
+                
+        
+                
+
+    
+        
+                        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                                XCUIApplication().launch()
+            }
+        }
+    }
+}
